cef9eb0272ca2faf2ea1123a7fcf8a0333f298f0
valentine salim <vp.mulia888@gmail.com>
2020-06-03 12:10:33 +0700

    all working feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,6 @@ class PagesController < ApplicationController
   def home
   end
 
+  def index
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,12 @@
 class Book < ApplicationRecord
+  CATEGORIES = ["Action", "Business", "Biography", "Children", "Crime", "Comic", "Education", "Fantasy", "Humor", "Horror", "Romance", "Mystery", "Satire", "Sci-Fi", "Self-help" ]
   belongs_to :owner, class_name: "User"
 
   has_many :rentals
 
   has_one_attached :photo
 
-  validates :title, :author, :description, :category, :rental_fee_per_day, presence: true
-
+  validates :title, :author, :description, :rental_fee_per_day, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   scope :rented, -> { where(rented: true) }
-
 end
