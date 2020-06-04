8f45e6b6f5042a4192f45edab31cc55d9588b048
valentine salim <vp.mulia888@gmail.com>
2020-06-04 10:35:40 +0700

    adding styles+rental controller+routes

---
+++ app/controllers/rental_controller.rb
@@ -0,0 +1,51 @@
+class RentalsController < ApplicationController
+    before_action :set_rental, only: [:show]
+    before_action :set_book, only: [:new, :create]
+    # A user can see the list of rentals
+    def index
+      @rentals = current_user.rentals
+    end
+  
+    # A user can create a new rental: GET + POST
+    # GET "rentals/new"
+    def new
+      @rental = Rental.new
+    end
+    # POST "rental"
+    def create
+      @rental = Rental.new(rental_params)
+      @rental.rentee = current_user
+      if @rental.save
+        redirect_to @rental
+      else
+        render 'new'
+      end
+    end
+  
+    # A user can see any rental page
+    # GET "rentals/id"
+    def show
+      @rental = Rental.find(params[:id])
+    end
+  
+    def destroy
+      @rental = Rental.find(params[:id])
+      @rental.destroy
+      redirect_to rentals_path, notice: 'Book Rental was successfully deleted.'
+    end
+  
+    private
+    def set_rental
+        @rental = Rental.find(params[:id])
+    end
+
+    def set_book
+        @book = Book.find(params[:book_id])
+    end
+
+    def rental_params
+      params.require(:rental).permit(:pickup_date, :return_date, :pickup_location)
+    end
+  
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books do
+    resources :rentals, only: [ :new, :create ]
+  end
   resources :pages
+  resources :rentals, only: [:show]
 end
